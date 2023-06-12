9b1830886d85f789edb1c955fdb9b1a7fb5a0e0e
JANNAHHASNAA <92834762+JANNAHHASNAA@users.noreply.github.com>
2023-06-12 21:24:07 +0800

    Update WorkoutRepository.java

--- src/main/java/com/csc3402/project/fittrackr/repositories/WorkoutRepository.java
+++ src/main/java/com/csc3402/project/fittrackr/repositories/WorkoutRepository.java
@@ -1,4 +1,13 @@
 package com.csc3402.project.fittrackr.repositories;
 
-public interface WorkoutRepository {
+import com.csc3402.project.fittrackr.model.Workout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkoutRepository extends JpaRepository<User, Integer> {
+    @Query(value = "SELECT * FROM workout where workout_id = :id", nativeQuery = true)
+   Workout findWorkoutById(@Param("id") int id);
 }
